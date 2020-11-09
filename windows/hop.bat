78a91236766009cb5c694bcb4ad4a8da6af8e317
House of Left <55319792+houseofleft@users.noreply.github.com>
2020-11-09 09:55:48 +0000

    Update hop.bat

--- windows/hop.bat
+++ windows/hop.bat
@@ -1,1 +1,1 @@
-"C:\Location\of\python\exe\Python\Python38-32\python.exe" "C:\where\you\downloaded\this\git\to\pacer\pacer.py" %*
+"C:\Location\of\python\exe\Python\Python38-32\python.exe" "C:\where\you\downloaded\this\git\to\hop\hop.py" %*
